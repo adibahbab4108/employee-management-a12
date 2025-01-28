941d00b34dde6fe2da9b2ba34fc915c2e6f9b12b
adibahbab4108 <adibalvi476@gmail.com>
2025-01-28 23:48:15 +0600

    Footer modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
     return (
         <div className="">
@@ -5,16 +7,13 @@ const Footer = () => {
                 <nav>
                     <h6 className="footer-title">Services</h6>
                     <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
                     <a className="link link-hover">Advertisement</a>
                 </nav>
                 <nav>
                     <h6 className="footer-title">Company</h6>
                     <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <Link to="contact-us">Contact</Link>
+                    
                 </nav>
                 <nav>
                     <h6 className="footer-title">Social</h6>
